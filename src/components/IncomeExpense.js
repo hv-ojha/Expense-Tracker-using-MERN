ae77d3f7c18eba1357bf2ead85ae69625aee4844
Ojha Harshvardhan Rajkumar <harshvardhan.rajkumar@hashedinx.com>
2020-05-27 16:57:15 +0530

    Delete transaction added

--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import { GlobalContext } from '../context/GlobalState'
 
 export const IncomeExpense = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map(transactions => transactions.amount);
+
+  const income = amounts.filter(amount => amount > 0).reduce((acc, amount) => (acc += amount), 0).toFixed(2);
+
+  const expense = (amounts.filter(amount => amount < 0).reduce((acc, amount) => (acc += amount), 0) * -1).toFixed(2);
+
     return (
       <div className="inc-exp-container">
         <div>
           <h4>Income</h4>
-          <p className="money plus">+$0.00</p>
+          <p className="money plus">${income}</p>
         </div>
         <div>
           <h4>Expense</h4>
-          <p className="money minus">-$0.00</p>
+          <p className="money minus">${expense}</p>
         </div>
       </div>
     )
