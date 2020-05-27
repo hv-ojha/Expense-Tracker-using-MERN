59e9becb23075002c8e2438b39e904063e34ade0
Ojha Harshvardhan Rajkumar <harshvardhan.rajkumar@hashedinx.com>
2020-05-27 15:24:29 +0530

    Transaction list is now displayed. Also implemented context api

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -6,12 +6,11 @@ export const AddTransaction = () => {
             <h3>Add new transaction</h3>
             <form>
                 <div className="form-control">
-                <label for="text">Text</label>
+                <label>Text</label>
                 <input type="text" placeholder="Enter text..." />
                 </div>
                 <div className="form-control">
-                <label for="amount"
-                    >Amount <br />
+                <label>Amount <br />
                     (negative - expense, positive - income)</label>
                 <input type="number" placeholder="Enter amount..." />
                 </div>
